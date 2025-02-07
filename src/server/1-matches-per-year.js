51f7a8341d32857d8110b5660617f5f4120c3e87
Ashwin Menghar <ashwin@Ashwins-MacBook-Air.local>
2025-02-07 21:28:11 +0530

    matches per year completed

---
+++ src/server/1-matches-per-year.js
@@ -0,0 +1,20 @@
+import { readFile, writeFile } from "./index.js";
+
+// Number of matches played per year for all the years in IPL.
+function numberOfMatchesPlayed(data) {
+  const res = data.reduce((acc, curr) => {
+    if (acc[curr.season] === undefined) acc[curr.season] = 1;
+    else acc[curr.season]++;
+    return acc;
+  }, {});
+
+  writeFile(res, "../public/output/1-matches-per-year.json");
+}
+
+readFile("../data/matches.json", (err, result) => {
+  if (err) {
+    console.log("Error reading file:", err.message);
+  } else {
+    numberOfMatchesPlayed(result);
+  }
+});
---
+++ src/server/index.js
@@ -0,0 +1,25 @@
+import fs from "fs";
+
+const writeFile = (data, fileName) => {
+  fs.writeFile(
+    fileName,
+    JSON.stringify(data, null, 2),
+    { encoding: "utf8" },
+    (err) => {
+      if (err) console.log(err.message);
+      else console.log("File written successfully\n");
+    }
+  );
+};
+
+const readFile = (filePath, cb) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, JSON.parse(data));
+    }
+  });
+};
+
+export { writeFile, readFile };
