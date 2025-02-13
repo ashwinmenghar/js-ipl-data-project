488a9724345f3dacee186b21fbd5f739066ae286
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-02-13 21:15:51 +0530

    Number of teams won toss as well as match completed using loops

--- src/server/5-find-number-of-times-team-won-toss-and-won-match.js
+++ src/server/5-find-number-of-times-team-won-toss-and-won-match.js
@@ -1,11 +1,13 @@
 // Find the number of times each team won the toss and also won the match
 const getAllTeamsWhoWonMatchAndToss = (matches) => {
-  return matches.reduce((teamsWonTossAndMatch, { toss_winner, winner }) => {
-    if (toss_winner === winner) {
+  let teamsWonTossAndMatch = {};
+
+  for (let { toss_winner, winner } of matches) {
+    if (toss_winner == winner) {
       teamsWonTossAndMatch[winner] = (teamsWonTossAndMatch[winner] ?? 0) + 1;
     }
-    return teamsWonTossAndMatch;
-  }, {});
+  }
+  return teamsWonTossAndMatch;
 };
 
 export default getAllTeamsWhoWonMatchAndToss;
