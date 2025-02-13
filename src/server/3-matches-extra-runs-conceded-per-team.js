791391b9ae43d8c6ae4374f0fbd1baaca02f76d4
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-02-13 19:40:58 +0530

    Matches extra runs by team in a season

--- src/server/3-matches-extra-runs-conceded-per-team.js
+++ src/server/3-matches-extra-runs-conceded-per-team.js
@@ -2,16 +2,18 @@ import { getMatchesId } from "./helper.js";
 
 const getExtraRunsByTeamInaSeason = (matches, deliveries, season) => {
   const matchIds = getMatchesId(matches, season);
+  let extraRunsByTeam = {};
 
-  return deliveries.reduce((acc, delivery) => {
-    if (matchIds.has(String(delivery.match_id))) {
+  for (let delivery of deliveries) {
+    if (matchIds.has(delivery.match_id)) {
       const team = delivery.bowling_team;
       const extraRuns = Number(delivery.extra_runs) || 0;
 
-      acc[team] = (acc[team] || 0) + extraRuns;
+      extraRunsByTeam[team] = (extraRunsByTeam[team] || 0) + extraRuns;
     }
-    return acc;
-  }, {});
+  }
+
+  return extraRunsByTeam;
 };
 
 export default getExtraRunsByTeamInaSeason;
