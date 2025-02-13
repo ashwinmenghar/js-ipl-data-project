6ef01bc35dd0c38f41b66f7e4735151f51da549a
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-02-13 22:01:35 +0530

    Max times player dismissed by bowler completed using loops

--- src/server/8-maximum-times-player-dismissed-by-a-bowler.js
+++ src/server/8-maximum-times-player-dismissed-by-a-bowler.js
@@ -5,27 +5,28 @@ const getHighestNumberOfTimesDismissedPlayers = (deliveries) => {
 };
 
 const getDismissedResult = (deliveries) => {
-  return deliveries.reduce((result, { batsman, bowler, dismissal_kind }) => {
+  let result = {};
+  for (const { batsman, bowler, dismissal_kind } of deliveries) {
     if (dismissal_kind) {
       result[bowler] = result[bowler] || {};
       result[bowler][batsman] = (result[bowler][batsman] || 0) + 1;
     }
-    return result;
-  }, {});
+  }
+  return result;
 };
 
 const findMostDismissedPlayer = (dismissedResult) => {
   let highestNumOfDismissedPlayer = "";
   let dismissedTimes = 0;
 
-  Object.entries(dismissedResult).forEach(([bowler, batsmanStats]) => {
-    Object.entries(batsmanStats).forEach(([batsman, count]) => {
+  for (const [bowler, batsmanStats] of Object.entries(dismissedResult)) {
+    for (const [batsman, count] of Object.entries(batsmanStats)) {
       if (count > dismissedTimes) {
         dismissedTimes = count;
         highestNumOfDismissedPlayer = batsman;
       }
-    });
-  });
+    }
+  }
 
   return {
     player: highestNumOfDismissedPlayer,
